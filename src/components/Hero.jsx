45bd4201a8c7ee8c7d9a4e03aa1a5b846d1aec5a
dczer <czerwinskidavid1@gmail.com>
2023-04-24 20:44:30 -0700

    init mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import foodImg from '../assets/Frame_20.svg'
+import banner from '../assets/banner.svg'
 
 export default function Hero() {
   return (
@@ -8,11 +9,12 @@ export default function Hero() {
             <hgroup>
             <h2 className='display-text secondary-text'>Little Lemon</h2>
             <p className='sub-title'> Chicago</p>
-            <p className='lead-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi illo magnam sequi enim quod? Facere, dolorum quam!</p>
             </hgroup>
+            <p className='lead-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi illo magnam sequi enim quod? Facere, dolorum quam!</p>
             <button className='hero-btn section-title' onClick={()=>console.log('reserve-btn')}>RESERVE A TABLE</button>
           </div>
-          <img src={foodImg} alt='restaurant food'/>
+          <img src={foodImg} alt='restaurant food' className='restaurant-food'/>
+          <img src={banner} alt='restaurant food' className='banner'/>
         </section>
   )
 }
